53097645a73818772e759b63951c93138925ca5f
lochungtin <lochungtin@gmail.com>
2021-03-17 16:55:37 +0800

    RN vector icon test build

--- App.js
+++ App.js
@@ -2,13 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-    return (
-        <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
-            <StatusBar style="auto" />
-        </View>
-    );
+export default class App extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
