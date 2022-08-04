39afb7afd54c39d8d2f2d371f50e36afdd9dd779
lochungtin <lochungtin@gmail.com>
2022-08-04 23:03:16 +0100

    clear

--- src/data/color.js
+++
@@ -1,6 +0,0 @@
-export const accent = '#9b224d';
-export const bgColor = '#0e0e0e';
-export const darkGrey = '#1e1e1e';
-export const grey = '#3e3e3e';
-export const lightGrey = '#6e6e6e';
-export const white = '#ffffff';
